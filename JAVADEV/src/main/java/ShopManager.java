c8e741a50e823dcd2693afeab77869167aed2d37
Shymal Poddar <shyamal8519@gmail.com>
2016-11-07 00:16:52 +0530

    Update ShopManager.java
    Code Cleanup

--- JAVADEV/src/main/java/ShopManager.java
+++ JAVADEV/src/main/java/ShopManager.java
@@ -2,5 +2,5 @@ import java.util.Map;
 
 interface ShopManager{
 	public Map<String,Address> constructAddressMap(String jsonString) throws Exception;
-	public String findNearestAdresses(Map<String,Address> addressMap,double currentLat,double currentLon);
+	public String findNearestAddresses(Map<String,Address> addressMap,double currentLat,double currentLon);
 }
