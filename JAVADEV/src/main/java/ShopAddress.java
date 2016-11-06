8e8d32c1144e3193edb381e70f78628eaa6f561a
shyamal123 <shyamal8519@gmail.com>
2016-11-06 22:58:39 +0530

    Add files via upload
    Spring boot Retail Manager project .

---
+++ JAVADEV/src/main/java/ShopAddress.java
@@ -0,0 +1,42 @@
+
+public class ShopAddress {
+	private String shopName;
+	private String shopAddressNumber;
+
+	private String 	shopAddressPostCode;
+	private String lat;
+	private String lon;
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopAddressNumber() {
+		return shopAddressNumber;
+	}
+	public void setShopAddressNumber(String shopAddressNumber) {
+		this.shopAddressNumber = shopAddressNumber;
+	}
+	public String getShopAddressPostCode() {
+		return shopAddressPostCode;
+	}
+	public void setShopAddressPostCode(String shopAddressPostCode) {
+		this.shopAddressPostCode = shopAddressPostCode;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	
+	   
+
+}
